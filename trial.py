459ce15380161687306cc39c4081933400a44108
Lennice Bolton <lennicebolton03@gmail.com>
2024-04-20 20:07:09 -0400

    Finished implementing backend functionality for the pptx->questions implementation

--- trial.py
+++ trial.py
@@ -1,4 +1,8 @@
 from pptx import Presentation
+import google.generativeai as genai
+from google.generativeai import GenerativeModel
+import requests
+
 
 def text_extraction(ppt_file):
     presentation = Presentation(ppt_file)
@@ -14,9 +18,35 @@ def text_extraction(ppt_file):
 
     return combined_text
 
-# Example usage
-ppt_file = "slides/h.pptx"
-extracted_text = text_extraction(ppt_file)
-print(extracted_text)
+#apikey = "AIzaSyCJ88aztjeTldBwJCF4QxqrbyzLINqlzBY"
+apikey="AIzaSyBmXC-dYqs0uVwonBYFYAuEPh0IzN-ACBE"
+
+def generate_questions_geminai(text):
+    # Replace "YOURAPIKEY" with your actual API key
+
+    # Configure the SDK with your API key
+    genai.configure(api_key=apikey)
+
+    # Create a model instance for Gemini
+    model = GenerativeModel("gemini-pro")
+
+    # Generate text from a prompt
+    prompt = "Write 5 multiple choice questions about " + text
+    response = model.generate_content(prompt)
+   # response = str(response)
+    #data = response.json()
+    print(response)
+    print("-------------------------------------------")
+    print(response.text)
 
 
+
+def main():
+    ppt_file = "slides/h.pptx"
+    extracted_text = text_extraction(ppt_file)
+    generate_questions_geminai(extracted_text)
+    #for i, question in enumerate(questions, 1):
+     #   print(f"Question {i}: {question}")
+
+if __name__ == "__main__":
+    main()
